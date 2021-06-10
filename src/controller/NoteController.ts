7beb0adb96b5e2436a6c194c643886ac2089c532
Jordan Radushev <63496500+JordanRad@users.noreply.github.com>
2021-06-10 15:54:02 +0200

    Note service finished. POST,PUT & DELETE added.

--- src/controller/NoteController.ts
+++ src/controller/NoteController.ts
@@ -1,37 +1,95 @@
 import express from 'express';
 import { Note } from '../model/Note';
 import { PrismaClient } from '@prisma/client';
-import {NoteService} from '../service/NoteService';
+import { NoteService } from '../service/NoteService';
+import { errorLog } from '../logging';
 const router = express.Router();
 
 router.get("/shared/:meetingId", async (req, res) => {
 
-    let meetingId = parseInt(req.params.meetingId)
+    let meetingId: number = parseInt(req.params.meetingId)
 
-    let notes = await NoteService.getSharedNotesPerMeeting(meetingId)
+    let notes: Note[] | undefined =
+        await NoteService.getSharedNotesPerMeeting(meetingId)
 
-    if(notes==undefined){
+    if (notes == undefined) {
+        return res.sendStatus(404)
+    }
+
+    return res.json(notes)
+
+})
+router.get("/shared/:meetingId/topic/:topic", async (req, res) => {
+    let meetingId: number = parseInt(req.params.meetingId)
+
+    let topic: string = req.params.topic
+
+    //So that "TOPIC" & TOPIC can be both recognised 
+    topic = topic.split("\"").join("")
+
+    let notes: Note[] | undefined =
+        await NoteService.getNotesByMeetingAndTopic(meetingId, topic)
+
+    if (notes == undefined) {
         return res.sendStatus(500)
     }
 
     return res.json(notes)
-    
 })
 router.get('/private/:meetingId/:userId', async (req, res) => {
-    
-    let meetingId = parseInt(req.params.meetingId)
-    console.log(meetingId)
-    let userId = parseInt(req.params.userId)
 
-    let notes = await NoteService.getPrivateNotesPerMeeting(meetingId,userId)
+    let meetingId: number = parseInt(req.params.meetingId)
 
-    if(notes==undefined){
-        return res.sendStatus(500)
+    let userId: number = parseInt(req.params.userId)
+
+    let notes: Note[] | undefined =
+        await NoteService.getPrivateNotesPerMeeting(meetingId, userId)
+
+    if (notes == undefined) {
+        return res.sendStatus(404)
     }
 
     return res.json(notes)
 })
 router.post("/", async (req, res) => {
-    
+    let note: Note = req.body;
+    console.log("POST Request here ", note)
+    let createdNote: Note | undefined =
+        await NoteService.createNote(note)
+
+    if (createdNote == undefined) {
+        return res.sendStatus(409)
+    }
+
+    return res.json(createdNote);
+
 })
+
+router.put("/:id", async (req, res) => {
+    let id: number = parseInt(req.params.id)
+    let note: Note = req.body;
+
+    let updatedNote: Note | undefined =
+        await NoteService.updateNote(id, note)
+
+    if (updatedNote == undefined) {
+        return res.sendStatus(404)
+    }
+
+    return res.json(updatedNote);
+})
+
+router.delete("/:id", async (req, res) => {
+    let id: number = parseInt(req.params.id)
+
+    let deletedNote: Note | undefined =
+        await NoteService.deleteNote(id)
+
+    if (deletedNote == undefined) {
+        return res.sendStatus(404)
+    }
+
+    return res.json(deletedNote);
+})
+
 export default router;
--- src/service/NoteService.ts
+++ src/service/NoteService.ts
@@ -18,13 +18,14 @@ export class NoteService {
             return notes != null ? notes : []
         } catch (err) {
             errorLog(err)
+            return undefined
         }
 
     }
 
-     /**
-     * Get shared notes per meeting by meeting id
-     */
+    /**
+    * Get shared notes per meeting by meeting id
+    */
     public static async getSharedNotesPerMeeting(meetingId: number) {
         try {
             let notes = await prisma.note.findMany({
@@ -35,10 +36,84 @@ export class NoteService {
             return notes != null ? notes : []
         } catch (err) {
             errorLog(err)
+            return undefined
         }
     }
 
-    public static getNotesByMeetingTopic(meetingId: number, topic: string): Note[] {
-        return [];
+
+    /**
+    * Get shared notes per meeting by meeting id and filtered by topic
+    */
+    public static async getNotesByMeetingAndTopic(meetingId: number, topic: string) {
+
+        try {
+            let notes = await prisma.note.findMany({
+                where: {
+                    meetingId: { equals: meetingId },
+                    type: { equals: "SHARED" },
+                    topic: { equals: topic }
+                },
+                include: { user: true },
+                orderBy: { createdAt: "asc" }
+            })
+            return notes != null ? notes : []
+        } catch (err) {
+            errorLog(err)
+            return undefined
+        }
+    }
+
+    /**
+     * Create note
+     */
+    public static async createNote(note: Note) {
+
+        try {
+            let createdNote = await prisma.note.create({
+                data: note
+            })
+
+            return createdNote;
+
+        } catch (err) {
+            errorLog(err)
+            return undefined
+        }
+    }
+
+    /**
+     * Update note
+     */
+    public static async updateNote(id: number, note: any) {
+        try {
+            let updatedNote = await prisma.note.update({
+                where: { id: id },
+                data: note
+            })
+
+            return updatedNote;
+
+        } catch (err) {
+            errorLog(err)
+            return undefined
+        }
+
+    }
+
+    /**
+     * Delete note
+     */
+    public static async deleteNote(id: number) {
+        try {
+            let deletedNote = await prisma.note.delete({
+                where: { id: id }
+            })
+
+            return deletedNote;
+
+        } catch (err) {
+            errorLog(err)
+            return undefined
+        }
     }
 }
